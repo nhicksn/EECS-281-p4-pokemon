8bf976300bb2f288de46520eb0e2acb469537211
Noah  Hicks <nhicksn@umich.edu>
2023-03-30 02:04:08 -0400

    added main files

---
+++ pokemon.cpp
@@ -0,0 +1,13 @@
+// Project Identifier: 5949F553E20B650AB0FB2266D3C0822B13D248B0
+
+#include "pokemon.h"
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    cout << std::setprecision(2); //Always show 2 decimal places
+    cout << std::fixed; //Disable scientific notation for large numbers
+}
---
+++ pokemon.h
@@ -0,0 +1,10 @@
+// Project Identifier: 5949F553E20B650AB0FB2266D3C0822B13D248B0
+
+#define POKEMON_H
+#ifndef POKEMON_H
+
+class MST {
+
+};
+
+#endif
